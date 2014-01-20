d86655a4ea37f07e13bc5de20e6f0e3ea2cf4985
Arnon Moscona <arnon@moscona.com>
2014-01-20 17:01:19 +0000

    started project

---
+++ src/main/java/com/moscona/common/App.java
@@ -0,0 +1,13 @@
+package com.moscona.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
